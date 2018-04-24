1eb2be06b06c0a95832c0347c59c074790c5e9c2
Jeremy Burns <j@jaburns.net>
2018-04-23 22:15:28 -0600

    Parsing all VB6 source in to AST

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,63 @@ extern crate pom;
 mod ast;
 mod parser;
 
-use std::fs::File;
+use std::fs::*;
 use std::io::prelude::*;
+use std::collections::HashMap;
+
+use ast::*;
 use parser::*;
 
-fn main() {
-    let mut file = File::open("vb6/Source/mGlobal.bas").unwrap();
+type Program = HashMap<String, Vec<TopLevelBlock>>;
 
+fn find_module_name(module: &Vec<TopLevelBlock>) -> Option<String> {
+    for item in module {
+        match item { 
+            &TopLevelBlock::Attribute {
+                 ref name,
+                 ref value,
+             } => {
+                if name == "VB_Name" {
+                    println!("Parsed module name: {}", value);
+                    return Some(value.clone());
+                }
+            }
+            _ => {}
+        };
+    }
+
+    None
+}
+
+fn read_file(path: &str) -> (String, Vec<TopLevelBlock>) {
+    let mut file = File::open(path).unwrap();
     let mut contents = String::new();
     file.read_to_string(&mut contents).unwrap();
 
-    let parsed = parse_module(contents.as_str());
+    let result = parse_module(contents.as_str());
 
-    for p in parsed {
-        println!(":: {:?}", p);
+    (find_module_name(&result).expect("Module '{}' did not have attribute VB_Name!"), result)
+}
+
+fn load_program() -> Program {
+    let mut result = HashMap::new();
+
+    for maybe_path in read_dir("vb6/Source").unwrap() {
+        let path = maybe_path.unwrap().path();
+        let path_str = path.to_str().unwrap();
+
+        if path_str.ends_with(".bas") || path_str.ends_with(".cls") {
+            println!("Loading and parsing module at {}", path_str);
+            let (name, parsed) = read_file(path_str);
+            result.insert(name, parsed);
+        }
     }
+
+    result
+}
+
+fn main() {
+    load_program();
 
     println!("\nSUCCESS\n");
 }
