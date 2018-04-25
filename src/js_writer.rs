eab3fb596e144cd856e0b224ef00443561074818
Jeremy Burns <j@jaburns.net>
2018-04-25 17:59:26 -0600

    Parsing in to regions

--- src/js_writer.rs
+++ src/js_writer.rs
@@ -22,12 +22,12 @@ fn get_functions(blocks: &Vec<TopLevelBlock>, at_level: AccessLevel) -> Vec<&Top
         .collect()
 }
 
-fn write_let(indent: usize, decl: &VarDeclaration) -> String {
+fn write_let(decl: &VarDeclaration) -> String {
     match decl.kind {
         VarKind::AutoInstantiate =>
-            format!("{}let {} = new_{}();\n", " ".repeat(indent), decl.name.as_str(), decl.type_name.as_str()),
+            format!("let {} = new_{}();\n", decl.name.as_str(), decl.type_name.as_str()),
         _ =>
-            format!("{}let {};\n", " ".repeat(indent), decl.name.as_str()),
+            format!("let {};\n", decl.name.as_str()),
     }
 }
 
@@ -48,7 +48,7 @@ fn write_module_footer(module: &Module) -> String {
 }
 
 fn join_lines(lines: &Vec<String>) -> String {
-//  lines.iter().intersperse(",".to_string())
+    lines.iter().fold(String::new(), |acc, line| acc + "\n" + line) + "\n"
 }
 
 fn write_module(module: &Module) -> String {
@@ -57,16 +57,47 @@ fn write_module(module: &Module) -> String {
     let mut return_block: Vec<String> = Vec::new();
     let mut post_footer: Vec<String> = Vec::new();
 
-/*
-    let header = write_module_header(module)
-    let footer = write_module_footer(module);
-*/
-
-
     let public_fields = get_fields(&module.contents, AccessLevel::Public);
     let private_fields = get_fields(&module.contents, AccessLevel::Private);
     let public_functions = get_functions(&module.contents, AccessLevel::Public);
     let private_functions = get_functions(&module.contents, AccessLevel::Private);
+
+    for block in module.contents {
+        match block {
+            TopLevelBlock::Constant { access_level, name, value, .. } => {
+                match access_level {
+                    AccessLevel::Public => {
+                        pre_header.push(format!("const {} = {};", name.as_str(), value.as_str()));
+                    }
+                    AccessLevel::Private => {
+                        post_header.push(format!("const {} = {};", name.as_str(), value.as_str()));
+                    }
+                }
+            },
+
+            TopLevelBlock::Field { access_level, declaration } => {
+                if module.is_class {
+                    post_header.push(write_let(declaration));
+                    if access_level == AccessLevel::Public {
+                        return_block.push_str(&format!("get {0}() {{ return {0}; }},\n", declaration.name.as_str()));
+                        return_block.push_str(&format!("set {0}(x) {{ {0} = x; }},\n", declaration.name.as_str()));
+                    }
+                } else {
+                    match access_level {
+
+                            pre_header.push(format!("const {} = {};", name.as_str(), value.as_str()));
+                        }
+                        AccessLevel::Private => {
+                            post_header.push(format!("const {} = {};", name.as_str(), value.as_str()));
+                        }
+                    }
+                }
+
+            },
+
+            _ => {}
+        }
+    }
 
     if !module.is_class {
         for decl in &public_fields {
@@ -74,7 +105,6 @@ fn write_module(module: &Module) -> String {
         }
     }
 
-    result.push_str(write_module_header(module).as_str());
 
     if module.is_class {
         for decl in &public_fields {
@@ -86,16 +116,6 @@ fn write_module(module: &Module) -> String {
         result.push_str(write_let(2, decl).as_str());
     }
 
-    // TODO write private functions
-
-    result.push_str("\n  return {\n");
-
-    // TODO write public functions
-
-    result.push_str("  };\n");
-
-    result.push_str(write_module_footer(module).as_str());
-
     if !module.is_class {
         for block in &public_functions {
             if let TopLevelBlock::Function { name, .. } = block {
@@ -103,6 +123,20 @@ fn write_module(module: &Module) -> String {
             }
         }
     }
+
+
+    let mut result = String::new();
+
+    result.push_str(format!("// Module: {}", module.name.as_str()));
+    result.push_str(join_lines(&pre_header).as_str());
+    result.push_str(write_module_header(module).as_str());
+    result.push_str(join_lines(&post_header).as_str());
+    result.push_str("\nreturn {\n");
+    result.push_str(join_lines(&return_block).as_str());
+    result.push_str("};\n");
+    result.push_str(write_module_footer(module).as_str());
+    result.push_str(join_lines(&post_footer).as_str());
+
 
     result
 }
